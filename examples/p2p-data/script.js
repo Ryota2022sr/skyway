0a0abb51ff27fe5fe404f3ea38809c9d3350740e
Ryota2022sr <110247092+Ryota2022sr@users.noreply.github.com>
2022-08-15 23:57:52 +0900

    Update script.js

--- examples/p2p-data/script.js
+++ examples/p2p-data/script.js
@@ -86,7 +86,7 @@ const Peer = window.Peer;
 
     function onClickSend() {
       const data = localText.value;
-      if(data!=''){
+      if(localText.value.trim()){
         dataConnection.send(data);
         messages.textContent += `You: ${data}\n`;
         localText.value = '';
